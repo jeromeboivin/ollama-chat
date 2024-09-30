5d3bfb6c555eb0bdd7b5c78dca65ccc232ccdb9b
Jerome Boivin <boivin.jerome@gmail.com>
2024-09-30 21:52:51 +0200

    Fixed MemoryManager error when ChromaDB not running

--- ollama_chat.py
+++ ollama_chat.py
@@ -2201,7 +2201,11 @@ def run():
 
             if use_memory_manager:
                 load_chroma_client()
-                memory_manager = MemoryManager(memory_collection_name, chroma_client, current_model, embeddings_model, verbose_mode)
+
+                if chroma_client:
+                    memory_manager = MemoryManager(memory_collection_name, chroma_client, current_model, embeddings_model, verbose_mode)
+                else:
+                    use_memory_manager = False
             continue
 
         if user_input == "/model2":
