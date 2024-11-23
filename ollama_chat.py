35759a0192125227de6878f35f13f0da7ec6ab9e
Jerome BOIVIN <jerome.boivin@esker.com>
2024-11-23 12:08:31 +0100

    Add command autocompletion feature using readline

--- ollama_chat.py
+++ ollama_chat.py
@@ -3,12 +3,12 @@ import platform
 import tempfile
 from colorama import Fore, Style
 import chromadb
+import readline
 
 if platform.system() == "Windows":
     import win32clipboard
 else:
     import pyperclip
-    import readline
 
 import argparse
 import re
@@ -69,6 +69,23 @@ memory_collection_name = "memory"
 long_term_memory_file = "long_term_memory.json"
 
 stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+# List of available commands to autocomplete
+COMMANDS = [
+    "/context", "/index", "/verbose", "/cot", "/search", "/web", "/model",
+    "/model2", "/tools", "/save", "/collection", "/memory", "/remember",
+    "/memorize", "/forget", "/rmcollection", "/deletecollection", "/chatbot",
+    "/cb", "/file"
+]
+
+def completer(text, state):
+    global COMMANDS
+
+    """Autocomplete function for readline."""
+    options = [cmd for cmd in COMMANDS if cmd.startswith(text)]
+    if state < len(options):
+        return options[state]
+    return None
 
 def on_user_input(input_prompt=None):
     for plugin in plugins:
@@ -2263,6 +2280,9 @@ def run():
     
     default_model = None
     prompt_template = None
+
+    readline.set_completer(completer)
+    readline.parse_and_bind("tab: complete")  # Enable tab completion
 
     # If specified as script named arguments, use the provided ChromaDB client host (--chroma-host) and port (--chroma-port)
     parser = argparse.ArgumentParser(description='Run the Ollama chatbot.')
